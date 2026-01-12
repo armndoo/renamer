24a82a0c263fd88ea9625c311532845b6472236d
armndo <armando.andreani07@gmail.com>
2026-01-12 15:58:28 +0100

    added the prompt file and getting contents from the dir logic

---
+++ README.md
@@ -0,0 +1,9 @@
+# Renamer
+
+
+## WIP
+Simple python program that  renames the last image downloaded in the Downloads folder.
+
+I hated how people online called images. Like you could see "ada8d848124.jpg" and then its a tree. Just name it "tree.jpg" no.
+
+Just kidding I know why they do this. But I only made this for fun and personal use so..
--- renamer.py
+++ renamer.py
@@ -1,8 +1,25 @@
 import os
+import sys
 
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
-
 api_key = os.getenv("GEMINI_API_KEY")
-print(api_key)
+
+
+with open("./prompt.txt", "r") as file:
+    content = file.read()
+
+host = sys.argv[1]
+print("Image files in the Downloads folder of {} \n".format(host))
+directory = "/home/{}/Downloads/".format(host)
+extensions = [".png", ".jpg", ".ico", ".webp", ".svg"]
+
+
+try:
+    for file in os.listdir(directory):
+        if file.endswith(tuple(extensions)):
+            img = os.path.join(directory, file)
+except FileNotFoundError:
+    print("Directory not found! Probably the host name is wrong")
