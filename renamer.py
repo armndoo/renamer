c69c03a94cbd6fdab53ec7042d1c451092aaa31a
armndo <armando.andreani07@gmail.com>
2026-01-14 17:01:03 +0100

    finished renaming logic

--- renamer.py
+++ renamer.py
@@ -1,25 +1,30 @@
+import datetime
 import os
 import sys
+from pathlib import Path
 
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
-
+load_dotenv(api_key)
 
 with open("./prompt.txt", "r") as file:
-    content = file.read()
+    prompt = file.read()
 
+
+client = genai.Client()
 host = sys.argv[1]
 print("Image files in the Downloads folder of {} \n".format(host))
 directory = "/home/{}/Downloads/".format(host)
-extensions = [".png", ".jpg", ".ico", ".webp", ".svg"]
+extensions = [".png", ".jpg"]
 
-
-try:
-    for file in os.listdir(directory):
-        if file.endswith(tuple(extensions)):
-            img = os.path.join(directory, file)
-except FileNotFoundError:
-    print("Directory not found! Probably the host name is wrong")
+for file in os.listdir(directory):
+    if file.endswith(tuple(extensions)):
+        img_path = Path(os.path.join(directory, file))
+        img = client.files.upload(file=img_path)
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview", contents=[img, prompt]
+        )
+        new_path = img_path.with_name(f"{response.text}{img_path.suffix}")
+        os.rename(str(img_path), new_path)
